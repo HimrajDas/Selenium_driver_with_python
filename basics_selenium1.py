5f7049a6f4d50f5c2b6634feceb73c56d289e2a4
HIMRAJ <himrajdas7@gmail.com>
2023-03-31 21:49:57 +0530

    added Service to use the driver

--- basics_selenium1.py
+++ basics_selenium1.py
@@ -4,7 +4,9 @@ Simple hands on the selenium with help of python library
 
 '''
 from selenium import webdriver
-driver = webdriver.Chrome(executable_path="chromedriver.exe")    # to use the functionality of the web driver we need to create the object of the drive with specific browser which we want to use
+from selenium.webdriver.chrome.service import Service
+
+driver = webdriver.Chrome(service=Service("chromedriver.exe"))    # to use the functionality of the web driver we need to create the object of the drive with specific browser which we want to use
 driver.get("https://mohitpeshwani.github.io/crazyprogrammer/")   #to load the target url we user webdriver.get("url") to load it
 driver.maximize_window() #to maxiumize the windows
 driver.minimize_window() #to minimize the windows
